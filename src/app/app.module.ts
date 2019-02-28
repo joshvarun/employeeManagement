1412517fb15b90ceb822e31e33f17a954a1d0204
Varun Joshi <joshvarun@gmail.com>
2019-02-28 11:28:28 +0530

    Initial Setup & Component Creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EmployeesComponent } from './components/employees/employees.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { EditEmployeeComponent } from './components/edit-employee/edit-employee.component';
+import { EmployeeDetailComponent } from './components/employee-detail/employee-detail.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    EmployeesComponent,
+    SidebarComponent,
+    AddEmployeeComponent,
+    EditEmployeeComponent,
+    EmployeeDetailComponent,
+    LoginComponent,
+    RegisterComponent,
+    SettingsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
