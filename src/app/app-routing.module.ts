6e9ac03fe312069a0906aa5c0a084e05b3e2a44c
Varun Joshi <joshvarun@gmail.com>
2019-02-28 21:33:05 +0530

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { EditEmployeeComponent } from './components/edit-employee/edit-employee.component';
+import { EmployeeDetailComponent } from './components/employee-detail/employee-detail.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: DashboardComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'employee/add', component: AddEmployeeComponent},
+  {path: 'employee/edit/:id', component: EditEmployeeComponent},
+  {path: 'employee/:id', component: EmployeeDetailComponent},
+  {path: 'settings', component: SettingsComponent},
+  {path: '**', component: NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
